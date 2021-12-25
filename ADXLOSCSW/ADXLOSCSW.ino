fd876975e2e24640ece056260a17d5ad9731527a
gembaware <albin.giraud@gembaware.com>
2021-12-25 18:34:46 +0100

    ECHEC

--- ADXLOSCSW/ADXLOSCSW.ino
+++ ADXLOSCSW/ADXLOSCSW.ino
@@ -1,4 +1,19 @@
+#include <MovingAverage.h>
 #include "PeakDetection.h"
+#include <Preferences.h>
+#include "hlp_preferences.h"
+#include <ETH.h>
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiClient.h>
+#include <WiFiGeneric.h>
+#include <WiFiMulti.h>
+#include <WiFiScan.h>
+#include <WiFiServer.h>
+#include <WiFiSTA.h>
+#include <WiFiType.h>
+#include <WiFiUdp.h>
+#include <ArduinoOSC.h>
 #include "hlp_wifi.h"
 #include "hlp_osc.h"
 #include "hlp_adxl.h"
@@ -9,21 +24,18 @@ Serial.begin(115200);  // JT
 delay(1000);
 Serial.println("Début Setup");
 
-pinMode(BPpin, INPUT_PULLUP);
 pref.begin("sauve", false);
-
-// Mutex
-
-
 // Paramétrage des services
 setupWifi();
+setupOSC();
 
 // Création des tâches
 xTaskCreatePinnedToCore(tskAcquisition,"Acquisition",10000,NULL,8,&Acquisition,0);
+//xTaskCreatePinnedToCore(tskParseOSC,"OSC",10000,NULL,8,&OSC,1);
+
 }
 
 void loop()
-
 {
-  
+  delay(100);
 }
--- ADXLOSCSW/hlp_osc.h
+++ ADXLOSCSW/hlp_osc.h
@@ -1,4 +1,101 @@
 // OSC
-//const char* host = "192.168.1.16";
-const char* host = "192.168.43.224";
+const char* host = "192.168.1.16";
+const char* interne = "192.168.1.31";
+const int port_interne = 11999;
+//const char* host = "192.168.43.224";
 const int send_port = 6448;
+const int receive_port = 12000;
+
+int zx, zy, zz; // Mesure temps zéro
+int px, py, pz; // Mesure temps -1
+int x, y, z; // Mesure calibrée
+int vx,vy,vz; // vitesse 
+int peakx,peaky,peakz; // pic
+int mabsx, mabsy, mabsz; // JT trouver les max en valeur absolue pour calibrer
+PeakDetection peakDetectionX,peakDetectionY,peakDetectionZ;
+int fenetreMoyenne = 20;
+MovingAverage<int> moyenneX(fenetreMoyenne);
+MovingAverage<int> moyenneY(fenetreMoyenne);
+MovingAverage<int> moyenneZ(fenetreMoyenne);
+
+
+
+TaskHandle_t OSC;
+
+void tskParseOSC (void * pvparameters) {
+  Serial.print("Début tâche OSC");
+  for (;;) {
+    delay(100);
+    OscWiFi.parse();
+  }
+}
+
+
+void setupOSC(){
+OscWiFi.subscribe(receive_port, "/statut",[](const OscMessage& m){
+        Serial.println(m.remoteIP());
+        Serial.println(m.remotePort());
+        Serial.println(m.address());
+        Serial.println(" adresse ADLX ");
+        Serial.print(WiFi.localIP());
+}
+ ); 
+
+
+peakDetectionX.begin(); // A tuner
+peakDetectionY.begin(); // A tuner
+peakDetectionZ.begin(); // A tuner
+
+
+
+/*
+ * Traitement des données brutes et envoi à l'extérieur
+ */
+OscWiFi.subscribe(port_interne, "/interne",[](const OscMessage& m){
+
+int rx,ry,rz;
+
+// On traite l'axe X         
+        px = x; // On sauvegarder la valeur n-1
+        rx = m.arg<int>(1); // On actualise la valeur n
+        x=rx-zx; // On ajoute le zéro de référence
+        x=constrain(map(x,-(mabsx-zx), mabsx-zx, 0, 127),0 , 127); // On mappe sur 0/127
+//        vx=abs((x-px)/(momentpresent-momentpasse)); // On calcule la vitesse
+        vx=abs(x-px); // On calcule la vitesse
+        moyenneX.push(x); // On met à jour la moyenne glissante
+        peakDetectionX.add((double)x); // On met à jour la pile pour la détection du pic.
+        peakx = peakDetectionX.getPeak(); // On tente une détection
+
+// On refait la même chose pour l'axe Y
+        py = y; // On sauvegarder la valeur n-1
+        ry = m.arg<int>(2); // On actualise la valeur n
+        y=ry-zy; // On ajoute le zéro de référence
+        y=constrain(map(y,-(mabsy-zy), mabsy-zy, 0, 127),0 , 127); // On mappe sur 0/127
+//        vy=abs((y-py)/(momentpresent-momentpasse)); // On calcule la vitesse
+        vy=abs(y-py); // On calcule la vitesse
+        moyenneY.push(y); // On met à jour la moyenne glissante
+        peakDetectionY.add((double)y); // On met à jour la pile pour la détection du pic.
+        peaky = peakDetectionY.getPeak(); // On tente une détection
+
+// On refait la même chose pour l'axe Z
+        pz = z; // On sauvegarder la valeur n-1
+        rz = m.arg<int>(3); // On actualise la valeur n
+        z=rz-zz; // On ajoute le zéro de référence
+        z=constrain(map(z,-(mabsz-zz), mabsz-zz, 0, 127),0 , 127); // On mappe sur 0/127
+//        vz=abs((z-pz)/(momentpresent-momentpasse)); // On calcule la vitesse
+        vz=abs(z-pz); // On calcule la vitesse
+        moyenneZ.push(z); // On met à jour la moyenne glissante
+        peakDetectionZ.add((double)z); // On met à jour la pile pour la détection du pic.
+        peakz = peakDetectionZ.getPeak(); // On tente une détection
+
+// On compose et envoie le message OSC
+
+        OscWiFi.send(host, send_port, "/adxl",(float)millis(),(float)x,(float)vx,(float)peakx ,(float) moyenneX.get(),(float)rx,(float)y,(float)vy,(float) moyenneY.get(),(float)ry,(float)z,(float)vz,(float) moyenneZ.get(),(float)rz);
+        }
+
+//                OscWiFi.send(host, send_port, "/adxl",(float)millis(),(float)rx,(float)ry,(float)rz);
+//        }
+ ); 
+
+ 
+}
